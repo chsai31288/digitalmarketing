39394b005d4dbdf3bf7c5f91ffcee8053b5c86f1
chsai31288 <2000031288@kluniversity.in>
2024-12-28 13:08:57 +0530

    first commit

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+
+    const swiper = new Swiper('.cs-testimonialSlider', {
+        loop: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination2',
+            clickable: true,
+        },
+        effect: 'fade',
+    });
